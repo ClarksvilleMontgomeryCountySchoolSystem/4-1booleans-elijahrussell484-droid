bd6355025f6084b1696d6dec2f52fc0a02b33b07
elijahrussell484-droid <elijah.russell484@students.cmcss.net>
2025-12-05 07:50:44 -0600

    Update and rename task1.py to task.py

---
+++ task.py
@@ -0,0 +1,53 @@
+# Paste code below then Commit changes [green button]
+#--------Group 1----------
+# Question: "Are you wearing shoes right now?"
+
+is_wearing_shoes = False   # You are not wearing shoes.
+
+# Question: "Did you eat breakfast today?"
+ate_breakfast_1 = True    # You ate breakfast this morning.
+ate_breakfast_2 = False   # You did not eat breakfast this morning.
+
+# Question: "Are you feeling awake?"
+is_awake = True     # You are feeling awake
+
+
+# Question: "Do you have a phone with you?"
+has_phone = True     # You have a phone with you.
+
+
+# Question: "Are you sitting near the sink?"
+
+near_sink = False    # You are not sitting near the sink.
+
+# ------Group 2--------
+
+# Question: "Are you NOT wearing a hat?"
+not_wearing_hat = False     # You have a hat on your head.
+
+
+# Question: "Did you NOT forget your homework?"
+not_forgot_homework = False    # You forgot your homework.
+
+
+# Question: "Are you NOT tired?"
+not_tired = True   # You don't need a nap.
+
+
+# Question: "Do you NOT have a pencil?"
+not_have_pencil = False     # You have a pencil
+
+
+# Question: "Are you NOT sitting by the teacher's desk?"
+not_in_front = False     # " Yes and the fridge too."
+
+
+
+# REFLECTION: Discuss with your partner
+# Which variable names were easiest to work with? Why?
+'''#is_wearing_shoes was the easiest to work with because the variable is 
+asking if we are wearing shoes and we are, so we answered true.'''
+# Which were most confusing? Why?
+''''#not_in_front was the most confusing because the variable 
+does not say the location of where we are or not close to. If we didn't have the question 
+we wouldn't know if we were close to where it was asking about or not.'''
